af898db90cb863cdb85b3533094e0b723dbea3e0
Samuel Gustin <gustin.daniel1@gmail.com>
2020-07-06 05:02:33 +0300

    darknet support
    added darknet detection with yolo trained on custom dataset

--- Raspberry Pi 4/pc_server.py
+++ Raspberry Pi 4/pc_server.py
@@ -8,10 +8,11 @@ import numpy as np
 from darkflow.net.build import TFNet
 
 
-options = {"model": "cfg/yolo.cfg",
-           "load": "darkflow/bin/yolo.weights",
-           "threshold": 0.1,
+options = {"model": "cfg/yolov2-custom.cfg",
+           "load": "darkflow/bin/yolov2-custom.weights",
+           "threshold": 0.2,
            "gpu": 1.0}
+tresh = 0.7
 
 tfnet = TFNet(options)
 
@@ -20,23 +21,20 @@ def send_signal():
     requests.get('http://192.168.1.5:5000/found', timeout=5)
 
 
-found = 0
+while True:
+    r = requests.get('http://192.168.1.2:5000/image.jpg', timeout=5)
+    curr_img = Image.open(BytesIO(r.content))
+    curr_img_cv2 = cv2.cvtColor(np.array(curr_img), cv2.COLOR_RGB2BGR)
 
-while found == 0:
-    r = requests.get('http://192.168.1.5:5000/image.jpg', timeout=5)
-    img_bytes = Image.open(BytesIO(r.content))
-    img = cv2.cvtColor(np.array(img_bytes), cv2.COLOR_RGB2BGR)
-
-    result = tfnet.return_predict(img)
+    result = tfnet.return_predict(curr_img_cv2)
     if len(result) > 0:
         sorted_result = sorted(result, key=lambda i: i['confidence'], reverse=True)
-        print(sorted_result[0]['label'])
+        if sorted_result[0]['confidence'] > tresh:
+            print(sorted_result[0]['label'])
 
-        for pred in sorted_result:
-            if pred['label'] == 'book':
-                send_signal()
-                found = 1
-                break
+        if sorted_result[0]['label'] == 'book':
+            send_signal()
+            break
 
     # plt.imshow(curr_img)
     # plt.show()
